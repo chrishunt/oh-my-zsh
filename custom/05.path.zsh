3638d7e4998f3e3263b4920ff8e3d6e32b1a45b4
Chris Hunt <c@chrishunt.co>
2018-03-21 16:14:03 -0700

    Add QT libraries to path

--- custom/05.path.zsh
+++ custom/05.path.zsh
@@ -4,6 +4,9 @@ export PATH=/usr/local/mysql/bin:$PATH
 # PATH for MongoDB
 export PATH=/Applications/mongodb-osx-x86_64-2.0.0/bin:$PATH
 
+# PATH for QT5.5 libraries (needed for Capybara gem)
+export PATH=$(brew --prefix qt@5.5)/bin:$PATH
+
 # Add home dir scripts to the path
 export PATH=~/bin:$PATH
 
